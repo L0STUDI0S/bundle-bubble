3ea17ced285b48f94b4d2153a572f4786dd03d16
L0STUDI0S <wafflesulloa@gmail.com>
2025-01-09 16:14:24 -0800

    Add files via upload

---
+++ mods/Bundle_Bubble.js
@@ -0,0 +1,13 @@
+elements.Money = {
+	color: "#ff0000",
+	behavior: behaviors.POWDER,
+	category: "Special",
+	state: "solid",
+}
+
+elements.Crodium = {
+	color: "#ff0000",
+	behavior: behaviors.LIQUID,
+	category: "Liquids",
+	state: "solid",
+}
